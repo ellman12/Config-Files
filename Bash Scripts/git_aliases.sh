702aea2b82a74f3914eb382034890d4c89c494cc
Elliott DuCharme <ellduc4@gmail.com>
2024-08-18 12:02:24 -0500

    Organize git_aliases.sh a bit and refine reset aliases

--- Bash Scripts/git_aliases.sh
+++ Bash Scripts/git_aliases.sh
@@ -2,25 +2,29 @@
 
 alias gs="git status"
 
+
 #Log
 alias gl="git log --abbrev-commit --decorate"
 alias glo="git log --abbrev-commit --decorate --oneline"
 alias glg="git log --abbrev-commit --decorate --graph"
 alias glog="git log --abbrev-commit --decorate --oneline --graph"
 
-#Commit, push, pull
+
+#Commit
 alias ga="git add ."
 gc() { git commit -m "$1"; }
 alias gca="git commit --amend --no-edit"
 alias gcae="git commit --amend"
 
-alias gp="git push -q ; clear"
-alias gpf="git push -f -q ; clear"
+
+#Push
+alias gp="git push -q && clear"
+alias gpf="git push -f -q && clear"
 alias gfa="git add . && git commit --amend --no-edit && gpf"
 
-alias gpl="git pull -q ; clear"
-grh() { git fetch && git reset --hard origin/"$1"; }
-gdb() { git branch -d "$1" && git push origin --delete "$1"; }
+
+#Pull
+alias gpl="git pull -q && clear"
 
 
 #Stash
@@ -29,5 +33,15 @@ alias gspu="git stash push"
 alias gspo="git stash pop"
 alias gsl="git stash list"
 gsd() { git stash drop "$1"; }
+
+
+#Reset
+alias grh="git reset --hard"
+grho() { git fetch && git reset --hard origin/"$1"; }
+
+
+#Branches
+gdb() { git branch -d "$1" && git push origin --delete "$1"; }
+
 
 
