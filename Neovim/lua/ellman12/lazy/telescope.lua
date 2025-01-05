c3e2b44e98aa07a1ee1ecfe44b6a5c8ee5d685c6
Elliott DuCharme <ellduc4@gmail.com>
2025-01-04 18:44:50 -0600

    Add telescope actions for oldfiles and live_grep

--- Neovim/lua/ellman12/lazy/telescope.lua
+++ Neovim/lua/ellman12/lazy/telescope.lua
@@ -18,5 +18,9 @@ return {
         vim.keymap.set("n", "<leader>fd", builtin.find_files, {})
 
         vim.keymap.set("n", "<leader>gf", builtin.git_files, {})
+
+        vim.keymap.set("n", "<leader>of", builtin.oldfiles, {})
+
+        vim.keymap.set("n", "<leader>gs", builtin.live_grep, {})
     end
 }
