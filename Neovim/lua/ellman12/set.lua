818be1de59fbb3d814dc98ec2ea7779e089bf720
Elliott DuCharme <ellduc4@gmail.com>
2025-01-07 11:43:54 -0600

    Neovim now properly sets the title of the terminal tab

--- Neovim/lua/ellman12/set.lua
+++ Neovim/lua/ellman12/set.lua
@@ -1,2 +1,7 @@
 vim.cmd [[source ~/Documents/GitHub/Config-Files/Vim/set.vim]]
 
+-- https://www.reddit.com/r/neovim/comments/13ia46q/comment/jk8sjk9/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+vim.opt.title = true
+vim.opt.titlelen = 0
+vim.opt.titlestring = "Neovim %{expand('%:t')}"
+
