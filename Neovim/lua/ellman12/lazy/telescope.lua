587d9cc379c6a42792dab0210318892d47cec901
Elliott DuCharme <ellduc4@gmail.com>
2025-03-26 12:56:10 -0500

    Add Q to do telescope git files search

--- Neovim/lua/ellman12/lazy/telescope.lua
+++ Neovim/lua/ellman12/lazy/telescope.lua
@@ -17,6 +17,7 @@ return {
 
         vim.keymap.set("n", "<leader>fd", builtin.find_files, {})
 
+        vim.keymap.set("n", "Q", builtin.git_files, {})
         vim.keymap.set("n", "<leader>gf", builtin.git_files, {})
 
         vim.keymap.set("n", "<leader>of", builtin.oldfiles, {})
